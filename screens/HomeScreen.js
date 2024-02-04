addb0491eee55e8e92c49a5b21f75213ab5d0bb2
talhaonder <ltalhaonderl@gmail.com>
2024-02-04 14:32:46 +0300

    first-one

---
+++ screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import { View, Text, SafeAreaView, StatusBar, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <SafeAreaView>
+        <StatusBar />
+        <View>
+            <Text>
+                <Text></Text>
+            </Text>
+            <TouchableOpacity>
+                
+            </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </View>
+  )
+}
