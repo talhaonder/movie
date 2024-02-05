ae16c9bfd243f478850b21c3e9ce519c144fdf10
talhaonder <ltalhaonderl@gmail.com>
2024-02-05 15:01:51 +0300

    api-1

--- api/moviedb.js
+++ api/moviedb.js
@@ -2,3 +2,23 @@ import axios from "axios";
 import {apiKey} from '../constants';
 
 //endpoints
+const apiBaseUrl= 'http://api.themoviedb.org/3';    
+const trendingMoviesEndpoint = `${apiBaseUrl}/trending/movie/week?api_key=${apiKey}`;
+
+const apiCall = async (endpoint, params)=>{
+    const options = {
+        method: "GET",
+        url: endpoint,
+        params: params? params: {}
+    }
+    try{
+        const response = await axios.request(options);
+        return response.data;
+    }catch(error){
+        console.log('error: ',error);
+        return{}
+    }
+}
+export const fetchTrendingMovies = ()=>{
+    return apiCall(trendingMoviesEndpoint);
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,36 @@
 import { View, Text, SafeAreaView, StatusBar, TouchableOpacity, Image, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Movies from '../components/Movies'
 import MovieList from '../components/MovieList'
 import { useNavigation } from '@react-navigation/native';
+import { fetchTrendingMovies } from '../api/moviedb';
 
 export default function HomeScreen() {
     const [movies, setMovies] = useState([1, 2, 3]);
     const [upcoming, setUpcoming] = useState([1, 2, 3]);
     const [topRated, setTopRated] = useState([1, 2, 3]);
     const navigation = useNavigation();
+
+    //checking api
+    useEffect(()=>{
+        getTrendingMovies();
+    },[])
+    const getTrendingMovies = async ()=>{
+        const data = await fetchTrendingMovies();
+        console.log('got trending movies: ', data);
+    }
     return (
         <View style={{ flex: 1, backgroundColor: 'rgb(30, 33, 29)' }}>
             <SafeAreaView>
                 <StatusBar />
                 <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", margin: 10 }}>
-                    <Image source={require('../assets/icons/like.png')} style={{
+                    <Image source={require('../assets/icons/user.png')} style={{
                         height: 25, width: 25,
                     }} />
                     <Text style={{ color: "rgb(31, 199, 155)", fontWeight: "700", fontSize: 24 }}>
                         <Text style={{ color: "white", fontWeight: "700" }}>Talh</Text>App
                     </Text>
-                    <TouchableOpacity onPress={()=> navigation.navigate('Search')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Search')}>
                         <Image source={require('../assets/icons/magnifying-glass.png')} style={{
                             height: 25, width: 25,
                         }} />
@@ -28,13 +38,10 @@ export default function HomeScreen() {
                 </View>
             </SafeAreaView>
             <ScrollView
-                contentContainerStyle={{ paddingBottom: 10 }}
-            >
+                contentContainerStyle={{ paddingBottom: 25,alignItems:"center" }}
+                style={{}}>
                 <Movies data={movies} />
-
-                <MovieList title="Upcoming" data={upcoming} />
-
-                <MovieList title="Top Rated" data={topRated} />
+                <Text style={{color: "rgb(142, 144, 143)"}}>Created at 02-05-2024 by. Talha Onder</Text>
             </ScrollView>
         </View>
     )
