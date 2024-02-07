f0da1290075b2e54ff97ad3d2ff6a0998a3466f9
talhaonder <ltalhaonderl@gmail.com>
2024-02-07 10:34:25 +0300

    +eslint

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,144 +1,175 @@
+import React, { useCallback, useState } from 'React';
 import {
-  View,
-  StyleSheet,
-  SafeAreaView,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-  Text,
-  TouchableWithoutFeedback,
-  Dimensions,
+    View,
+    StyleSheet,
+    SafeAreaView,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    Text,
+    TouchableWithoutFeedback,
+    Dimensions,
 } from 'react-native';
-import React, {useCallback, useState} from 'react';
-import {useNavigation} from '@react-navigation/native';
-import {fallbackMoviePoster, image185, searchMovies} from '../api/moviedb';
-import {debounce} from 'lodash';
-const {width, height} = Dimensions.get('window');
+import { useNavigation } from '@react-navigation/native';
+import { debounce } from 'lodash';
+import { fallbackMoviePoster, image185, searchMovies } from '../api/moviedb';
+
+const { width, height } = Dimensions.get('window');
 
 const SearchScreen = () => {
-  const navigation = useNavigation();
-  const [results, setResults] = useState([]);
-  let movieName = 'Oppenheimer biggest bomb in the world';
-  const handleSearch = value => {
-    if (value && value.length > 2) {
-      searchMovies({
-        query: value,
-        include_adult: 'false',
-        language: 'en-US',
-        page: '1',
-      }).then(data => {
-        //console.log('got movies: ', data);
-        if (data && data.results) setResults(data.results);
-      });
-    } else {
-      setResults([]);
-    }
-  };
-  const handleTextDebounce = useCallback(debounce(handleSearch, 400), []);
-  return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'rgb(30, 33, 29)'}}>
-      <View style={styles.container}>
-        <TextInput
-          onChangeText={handleTextDebounce}
-          placeholder="Search Movie"
-          placeholderTextColor={'lightgray'}
-          style={{
-            paddingBottom: 10,
-            paddingLeft: 15,
-            color: 'white',
-            fontWeight: '400',
-          }}
-        />
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('Home');
-          }}
-          style={{
-            borderRadius: 999,
-            padding: 12,
-            margin: 4,
-            backgroundColor: 'rgb(30, 33, 29)',
-          }}>
-          <Image
-            source={require('../assets/icons/back.png')}
-            style={{
-              height: 30,
-              width: 30,
-            }}
-          />
-        </TouchableOpacity>
-      </View>
-      {results.length > 0 ? (
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{paddingHorizontal: 15}}
-          style={{marginTop: 15}}>
-          <Text style={{color: 'white', fontWeight: '400', marginLeft: 4}}>
-            Results ({results.length})
-          </Text>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              flexWrap: 'wrap',
-            }}>
-            {results.map((item, index) => {
-              console.log('Item:', item);
-              return (
-                <TouchableWithoutFeedback
-                  key={index}
-                  onPress={() => navigation.push('Movie', item)}>
-                  <View style={{marginBottom: 16, marginTop: 8}}>
+    const navigation = useNavigation();
+    const [results, setResults] = useState([]);
+    const [movieName, setMovieName] = useState(
+        'Oppenheimer biggest bomb in the world',
+    );
+
+    const handleSearch = useCallback(value => {
+        if (value && value.length > 2) {
+            searchMovies({
+                query: value,
+                include_adult: 'false',
+                language: 'en-US',
+                page: '1',
+            }).then(data => {
+                if (data && data.results) {
+                    setResults(data.results);
+                }
+            });
+        } else {
+            setResults([]);
+        }
+    }, []);
+
+    const handleTextDebounce = useCallback(debounce(handleSearch, 400), []);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    onChangeText={handleTextDebounce}
+                    placeholder="Search Movie"
+                    placeholderTextColor="lightgray"
+                    style={styles.input}
+                />
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('Home');
+                    }}
+                    style={styles.backButton}>
                     <Image
-                      style={{
-                        borderRadius: 25,
-                        width: width * 0.44,
-                        height: height * 0.3,
-                      }}
-                      source={{
-                        uri: image185(item?.poster_path) || fallbackMoviePoster,
-                      }}
+                        source={require('../assets/icons/back.png')}
+                        style={styles.backIcon}
                     />
-                    <Text style={{color: 'rgb(142, 144, 143)', marginLeft: 4}}>
-                      {item?.title && item?.title.length > 22
-                        ? item?.title.slice(0, 22) + '...'
-                        : item?.title}
-                    </Text>
-                  </View>
-                </TouchableWithoutFeedback>
-              );
-            })}
-          </View>
-        </ScrollView>
-      ) : (
-        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-          <Image
-            source={require('../assets/image/movieTime.png')}
-            style={{
-              height: height * 0.5,
-              width: width,
-            }}
-          />
-        </View>
-      )}
-    </SafeAreaView>
-  );
+                </TouchableOpacity>
+            </View>
+            {results.length > 0 ? (
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={styles.resultsContainer}>
+                    <Text style={styles.resultsText}>Results ({results.length})</Text>
+                    <View style={styles.moviesContainer}>
+                        {results.map((item, index) => (
+                            <TouchableWithoutFeedback
+                                key={index}
+                                onPress={() => navigation.push('Movie', item)}>
+                                <View style={styles.movieItem}>
+                                    <Image
+                                        style={styles.movieImage}
+                                        source={{
+                                            uri: image185(item?.poster_path) || fallbackMoviePoster,
+                                        }}
+                                    />
+                                    <Text style={styles.movieTitle}>
+                                        {item?.title && item?.title.length > 22
+                                            ? item?.title.slice(0, 22) + '...'
+                                            : item?.title}
+                                    </Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        ))}
+                    </View>
+                </ScrollView>
+            ) : (
+                <View style={styles.noResultsContainer}>
+                    <Image
+                        source={require('../assets/image/movieTime.png')}
+                        style={styles.noResultsImage}
+                    />
+                </View>
+            )}
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 16, // mx-4
-    marginBottom: 3, // mb-3
-    flexDirection: 'row', // flex-row
-    justifyContent: 'space-between', // justify-between
-    alignItems: 'center', // items-center
-    borderColor: 'rgb(94, 97, 94))', // border-neutral-500 (örnek renk)
-    borderWidth: 1,
-    marginTop: 13,
-    borderRadius: 999, // rounded-full (büyük bir sayı ile daire yapar)
-    // border-width
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'rgb(30, 33, 29)',
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: 16,
+        marginBottom: 3,
+        marginTop: 13,
+        borderWidth: 1,
+        borderRadius: 999,
+        borderColor: 'rgb(94, 97, 94)',
+    },
+    input: {
+        paddingBottom: 10,
+        paddingLeft: 15,
+        color: 'white',
+        fontWeight: '400',
+        flex: 1,
+    },
+    backButton: {
+        borderRadius: 999,
+        padding: 12,
+        margin: 4,
+        backgroundColor: 'rgb(30, 33, 29)',
+    },
+    backIcon: {
+        height: 30,
+        width: 30,
+    },
+    resultsContainer: {
+        paddingHorizontal: 15,
+        marginTop: 15,
+    },
+    resultsText: {
+        color: 'white',
+        fontWeight: '400',
+        marginLeft: 4,
+    },
+    moviesContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+    },
+    movieItem: {
+        marginBottom: 16,
+        marginTop: 8,
+    },
+    movieImage: {
+        borderRadius: 25,
+        width: width * 0.44,
+        height: height * 0.3,
+    },
+    movieTitle: {
+        color: 'rgb(142, 144, 143)',
+        marginLeft: 4,
+    },
+    noResultsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    noResultsImage: {
+        height: height * 0.5,
+        width: width,
+    },
 });
 
 export default SearchScreen;
