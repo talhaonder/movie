937f48573ee3baf9505e6e38448e5980b6401bdb
talhaonder <ltalhaonderl@gmail.com>
2024-02-10 14:44:47 +0300

    refactor!: drop redux-saga

--- src/store/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import favoriteReducer from '../containers/Home/reducer';
-
-const rootReducer = combineReducers({
-  favorite: favoriteReducer,
-});
-
-export default rootReducer;
--- src/store/sagas.js
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-import favoriteSaga from '../containers/Home/saga';
-
-function* rootSaga() {
-  yield all([
-    favoriteSaga(),
-  ]);
-}
-export default rootSaga;
